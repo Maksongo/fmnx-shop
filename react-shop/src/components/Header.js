a6646e87d09c11121cdef9043716026b2561e2b8
Makson <nooplace55@gmail.com>
2023-11-18 01:15:57 +0600

    изменения в хедере

--- react-shop/src/components/Header.js
+++ react-shop/src/components/Header.js
@@ -3,14 +3,17 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-        <div>
+      <nav>
+          <div className="Header-logo-div">
+            <img className="header-logo" src="img/logo-icon.png" alt="" />
             <span className='logo'>FMNX Store</span>
-            <ul className='nav'>
+            </div>
+            <ul className='nava'>
               <li>Про нас</li>
               <li>Контакты</li>
               <li>Кабинет</li>
             </ul>
-        </div>
+            </nav>
         <div className='presentation'></div>
     </header>
   )
