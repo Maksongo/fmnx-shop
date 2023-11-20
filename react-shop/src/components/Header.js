012d8f8cde565ecf269fe57c0c2a5b21ee4ee986
Makson <nooplace55@gmail.com>
2023-11-20 23:12:59 +0600

    added cart in header

--- react-shop/src/components/Header.js
+++ react-shop/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
 
 export default function Header() {
   return (
@@ -8,11 +9,14 @@ export default function Header() {
             <img className="header-logo" src="img/logo-icon.png" alt="" />
             <span className='logo'>FMNX Store</span>
             </div>
+            <div className='header-nav-buttons'>
             <ul className='nava'>
               <li>Про нас</li>
               <li>Контакты</li>
               <li>Кабинет</li>
             </ul>
+            <FaCartShopping className='shop-cart-button' />
+            </div>
             </nav>
         {/* <div className='presentation'></div> */}
     </header>
