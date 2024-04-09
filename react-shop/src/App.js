20e608d37c1c076efe06a08d003f63104db451c9
Makson <nooplace55@gmail.com>
2024-04-09 22:08:09 +0600

    добавлена возможность фильтрации категории товаров

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,87 +1,97 @@
-import React from "react"
+import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 import Items from "./components/items";
-
-  
+import Categories from "./components/Categories";
 
 class App extends React.Component {
-
-constructor(props) {
-  super(props)
-  this.state = {
-    orders: [],
-    items: [
-      {
-        id: 1,
-        title: 'Ноутбук FMNX',
-        img: '/test.webp',
-        desc: 'Description Description Description Description ',
-        category: 'laptops',
-        price: '500'
-      },
-      {
-        id: 2,
-        title: 'Зарядка FMNX',
-        img: '/battarey.png',
-        desc: 'Description Description Description Description ',
-        category: 'laptops',
-        price: '500'
-      },
-      {
-        id: 3,
-        title: 'Монитор FMNX',
-        img: '/monitor.webp',
-        desc: 'Description Description Description Description ',
-        category: 'laptops',
-        price: `500`
-      },
-      {
-        id: 4,
-        title: 'Клавиатура FMNX',
-        img: '/keyboard.webp',
-        desc: 'Description Description Description Description ',
-        category: 'laptops',
-        price: `500`
-      },
-      {
-        id: 5,
-        title: 'Мышь FMNX',
-        img: '/mouse.webp',
-        desc: 'Description Description Description Description ',
-        category: 'laptops',
-        price: `500`
-      },
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: [],
+      currentItems: [],
+      items: [
+        {
+          id: 1,
+          title: "Ноутбук FMNX",
+          img: "/test.webp",
+          desc: "Description Description Description Description ",
+          category: "laptops",
+          price: "500",
+        },
+        {
+          id: 2,
+          title: "Зарядка FMNX",
+          img: "/battarey.png",
+          desc: "Description Description Description Description ",
+          category: "zaryadki",
+          price: "500",
+        },
+        {
+          id: 3,
+          title: "Монитор FMNX",
+          img: "/monitor.webp",
+          desc: "Description Description Description Description ",
+          category: "laptops",
+          price: `500`,
+        },
+        {
+          id: 4,
+          title: "Клавиатура FMNX",
+          img: "/keyboard.webp",
+          desc: "Description Description Description Description ",
+          category: "laptops",
+          price: `500`,
+        },
+        {
+          id: 5,
+          title: "Мышь FMNX",
+          img: "/mouse.webp",
+          desc: "Description Description Description Description ",
+          category: "laptops",
+          price: `500`,
+        },
+      ],
+    };
+    this.state.currentItems = this.state.items;
+    this.addToOrder = this.addToOrder.bind(this); // строчка позволяет в методе addToOrder взаимодействовать с состояниями
+    this.deleteOrder = this.deleteOrder.bind(this);
+    this.chooseCategory = this.chooseCategory.bind(this);
   }
-  this.addToOrder = this.addToOrder.bind(this) // строчка позволяет в методе addToOrder взаимодействовать с состояниями
-  this.deleteOrder = this.deleteOrder.bind(this)
-}
 
   render() {
-  return (
-    <div>
-    <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-    <div className="wrapper">
-      <Items items={this.state.items} onAdd={this.addToOrder} />
-    </div>
-    <Footer />
-    </div>
-  )
+    return (
+      <div>
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <div className="wrapper">
+          <Categories chooseCategory={this.chooseCategory} />
+          <Items items={this.state.currentItems} onAdd={this.addToOrder} />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+
+  chooseCategory(category) {
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items})
+      return
+    }
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    });
   }
 
   deleteOrder(id) {
-    this.setState({ orders: this.state.orders.filter(el => el.id !== id)})
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
   }
 
   addToOrder(item) {
     let isInArray = false;
-    this.state.orders.forEach(el =>{
-      if(el.id === item.id)
-      isInArray = true
-    })
-    if(!isInArray)
-    this.setState({orders: [...this.state.orders, item] }) // обращаемся к масиву orders, и добавляем в него item
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) isInArray = true;
+    });
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] }); // обращаемся к масиву orders, и добавляем в него item
   }
 }
 
